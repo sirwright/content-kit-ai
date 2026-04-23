a3a70b6e291e10c06c76d077686dbbee2d3046e6
sirwright <raphaelwright@gmail.com>
2026-04-23 17:51:54 -0300

    Create next.config.js

---
+++ app/app/app/gerar/app/api/generate/app/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // Basic webpack config - can be extended as needed
+    return config;
+  },
+};
+
+module.exports = nextConfig;
